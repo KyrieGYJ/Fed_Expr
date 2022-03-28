3fc6e92154eb05c5de7aed71eb2383c0ac2ba08b
KyrieG <2569854140@qq.com>
2022-03-28 13:35:13 +0800

    feat:添加fedavg论文采用的cnn

--- MyExpr/dfl/model/cnn.py
+++ MyExpr/dfl/model/cnn.py
@@ -1,4 +1,4 @@
-
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
@@ -63,3 +63,35 @@ class BaseConvNet(nn.Module):
         x = F.relu(self.fc2(x))
         if layer == 3:
             return self.fc3(x)
+
+
+class FedAvgCNN(nn.Module):
+    def __init__(self, in_features=1, num_classes=10, dim=1024):
+        super().__init__()
+        self.conv1 = nn.Conv2d(in_features,
+                               32,
+                               kernel_size=5,
+                               padding=0,
+                               stride=1,
+                               bias=True)
+        self.conv2 = nn.Conv2d(32,
+                               64,
+                               kernel_size=5,
+                               padding=0,
+                               stride=1,
+                               bias=True)
+        self.fc1 = nn.Linear(dim, 512)
+        self.fc = nn.Linear(512, num_classes)
+
+        self.act = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=(2, 2))
+
+    def forward(self, x):
+        x = self.act(self.conv1(x))
+        x = self.maxpool(x)
+        x = self.act(self.conv2(x))
+        x = self.maxpool(x)
+        x = torch.flatten(x, 1)
+        x = self.act(self.fc1(x))
+        x = self.fc(x)
+        return x
--- MyExpr/dfl/model/model_builder.py
+++ MyExpr/dfl/model/model_builder.py
@@ -4,6 +4,7 @@ from MyExpr.dfl.model.resnet import resnet50
 from MyExpr.dfl.model.resnet import resnet101
 from MyExpr.dfl.model.resnet import resnet152
 from MyExpr.dfl.model.cnn import BaseConvNet
+from MyExpr.dfl.model.cnn import FedAvgCNN
 
 def get_model_builder(args):
     model_builder = resnet18
@@ -19,4 +20,6 @@ def get_model_builder(args):
         model_builder = resnet152
     elif args.model == "BaseConvNet":
         model_builder = BaseConvNet
+    elif args.model == "FedAvgCNN":
+        model_builder = FedAvgCNN
     return model_builder
